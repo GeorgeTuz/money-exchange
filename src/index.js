94269064c343a56591309efd5aea8382a20eeeba
Heorhi Tuz <george_tuz@mail.ru>
2019-03-14 22:14:56 +0300

    feat: add function Money Exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var coins = {};
+    if (currency >= 10000) {
+        coins.error = "You are rich, my friend! We don't have so much coins for exchange";
+    } else if (currency > 0) {
+        if (currency >= 50){
+            coins.H = Math.trunc(currency / 50);
+            currency = currency % 50;
+        }
+        if (currency >= 25){
+            coins.Q = Math.trunc(currency / 25);
+            currency = currency % 25;
+        }
+        if (currency >= 10){
+            coins.D = Math.trunc(currency / 10);
+            currency = currency % 10;
+        }
+        if (currency >= 5){
+            coins.N = Math.trunc(currency / 5);
+            currency = currency % 5;
+        }
+        if (currency >= 1){
+            coins.P = currency;
+        }
+    }
+    return coins;
 }
